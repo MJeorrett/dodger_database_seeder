0d9379759800e6c6dbcc3c2f5fb9d11ebc3fbe1f
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-22 11:41:23 +0100

    databases_controlleradded and routes laid out

---
+++ controllers/databases_controller.rb
@@ -0,0 +1,27 @@
+get '/databases' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databases' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+get '/databases/new' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databases/new' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+post '/databases' do
+  "Error 600: Matthew laziness error<br /><br />POST '/databases' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+get '/databases/:dbname' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databases/:dbname' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+get '/databases/:dbname/edit' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databases/:dbname/edit' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+put '/databases/:dbname' do
+  "Error 600: Matthew laziness error<br /><br />PUT '/databases/:dbname' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+delete '/databases/:dbname' do
+  "Error 600: Matthew laziness error<br /><br />DELETE '/databases/:dbname' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
