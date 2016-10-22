6bb5030291191173041075934e09c678a69ab12a
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-22 13:38:55 +0100

    generate_table returns valid html but indentation is not great

--- models/html_table.rb
+++ models/html_table.rb
@@ -1,7 +1,27 @@
+require_relative('./html_element')
+
 class HtmlTable
 
   def self.generate_table( data )
-    
+
+    th_elements = data.first.keys.map do |column|
+      HtmlElement.new( 'th', column )
+    end
+
+    thead = HtmlElement.new( 'thead', th_elements )
+
+    tr_elements = data.map do |row|
+
+      td_elements = row.values.map do |value|
+        HtmlElement.new( 'td', value )
+      end
+
+      tr_element = HtmlElement.new( 'tr', td_elements )
+    end
+
+    table_contents = [ thead ] + tr_elements
+    table = HtmlElement.new( 'table', table_contents )
+    return table.to_s
   end
 
 end
