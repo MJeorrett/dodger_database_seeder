084a967a9a8ea3e0d87a20cd3b2bb01de8ef5301
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-22 12:20:39 +0100

    boolean attribute test written

--- models/specs/html_tag_spec.rb
+++ models/specs/html_tag_spec.rb
@@ -7,16 +7,27 @@ class HtmlTagTest < MiniTest::Test
 
   def test_simple_attributes_string_contents
 
-    a_tag_attributes = {
+    attributes = {
       id: 'this_link',
       class: 'important',
       href: 'www.html.com'
     }
 
-    @a_tag = HtmlTag.new('a', a_tag_attributes, "Click me!" )
+    a_element = HtmlTag.new('a', attributes, "Click me!" )
     expected = "<a id='this_link' class='important' href='www.html.com'>Click me!</a>"
 
-    assert_equal( expected, @a_tag.to_s )
+    assert_equal( expected, a_element.to_s )
+  end
+
+  def test_boolean_attribute
+    attributes = {
+      id: 'option_1',
+      selected: :boolean
+    }
+    option_element = HtmlTag.new( 'option', attributes, "Option 1")
+    expected = "<option id='option_1' selected>Option 1</option>"
+
+    assert_equal( expected, option_element.to_s )
   end
 
 end
