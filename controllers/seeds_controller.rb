d1fbacd04a9572bc3e7817d5da5b270d09fb63b7
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-22 18:32:30 +0100

    correcting route urls

--- controllers/seeds_controller.rb
+++ controllers/seeds_controller.rb
@@ -1,39 +1,37 @@
 require('sinatra')
 require('sinatra/contrib/all')
 
-require_relative('controllers/databases_controller')
-
 # INDEX
-get '/databases' do
-  "Error 600: Matthew laziness error<br /><br />GET '/databases' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+get '/databses/:dbname/seeds' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databses/:dbname/seeds' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
 end
 
 # NEW
-get '/databases/new' do
-  "Error 600: Matthew laziness error<br /><br />GET '/databases/new' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+get '/databses/:dbname/seeds/new' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databses/:dbname/seeds/new' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
 end
 
 # CREATE
-post '/databases' do
-  "Error 600: Matthew laziness error<br /><br />POST '/databases' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+post '/databses/:dbname/seeds' do
+  "Error 600: Matthew laziness error<br /><br />POST '/databses/:dbname/seeds' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
 end
 
 # SHOW
-get '/databases/:dbname' do
-  "Error 600: Matthew laziness error<br /><br />GET '/databases/:dbname' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+get '/databses/:dbname/seeds/:dbname' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databses/:dbname/seeds/:dbname' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
 end
 
 # EDIT
-get '/databases/:dbname/edit' do
-  "Error 600: Matthew laziness error<br /><br />GET '/databases/:dbname/edit' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+get '/databses/:dbname/seeds/:dbname/edit' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databses/:dbname/seeds/:dbname/edit' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
 end
 
 # UPDATE
-put '/databases/:dbname' do
-  "Error 600: Matthew laziness error<br /><br />PUT '/databases/:dbname' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+put '/databses/:dbname/seeds/:dbname' do
+  "Error 600: Matthew laziness error<br /><br />PUT '/databses/:dbname/seeds/:dbname' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
 end
 
 # DESTROY
-delete '/databases/:dbname' do
-  "Error 600: Matthew laziness error<br /><br />DELETE '/databases/:dbname' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+delete '/databses/:dbname/seeds/:dbname' do
+  "Error 600: Matthew laziness error<br /><br />DELETE '/databses/:dbname/seeds/:dbname' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
 end
