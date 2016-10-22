23127d52a432ba6a4c35e5432c33c985c3ec0a13
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-22 12:31:49 +0100

    moved object instantiation to setup method

--- models/specs/html_element_spec.rb
+++ models/specs/html_element_spec.rb
@@ -5,29 +5,35 @@ require_relative('../html_element')
 
 class HtmlElementTest < MiniTest::Test
 
-  def test_simple_attributes_string_contents
-
-    attributes = {
+  def setup()
+    a_attributes = {
       id: 'this_link',
       class: 'important',
       href: 'www.html.com'
     }
+    @a_element = HtmlElement.new('a', a_attributes, "Click me!" )
 
-    a_element = HtmlElement.new('a', attributes, "Click me!" )
-    expected = "<a id='this_link' class='important' href='www.html.com'>Click me!</a>"
-
-    assert_equal( expected, a_element.to_s )
-  end
-
-  def test_boolean_attribute
-    attributes = {
+    option_attributes = {
       id: 'option_1',
       selected: :boolean
     }
-    option_element = HtmlElement.new( 'option', attributes, "Option 1")
-    expected = "<option id='option_1' selected>Option 1</option>"
+    @option_element = HtmlElement.new( 'option', option_attributes, "Option 1")
+  end
 
+  def test_simple_attributes_string_contents()
+
+    expected = "<a id='this_link' class='important' href='www.html.com'>Click me!</a>"
+    assert_equal( expected, @a_element.to_s )
+  end
+
+  def test_boolean_attribute()
+
+    expected = "<option id='option_1' selected>Option 1</option>"
     assert_equal( expected, option_element.to_s )
+  end
+
+  def test_single_nested_element()
+
   end
 
 end
