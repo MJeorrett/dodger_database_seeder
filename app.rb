591839172a38eb388e28e2c16b8935744f475c16
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-23 09:48:54 +0100

    adding styling for no-data class

--- app.rb
+++ app.rb
@@ -4,4 +4,4 @@ require('sinatra/contrib/all')
 require_relative('controllers/databases_controller')
 require_relative('controllers/seeds_controller')
 
-NO_DATA_MESSAGE = "<h3 class='no-data'>-- No data --</h3>"
+NO_DATA_MESSAGE = "<p class='no-data'>-- No data --</p>"
