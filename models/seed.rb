9e7e2c23d8646cd5682f70fa44b223fd26ac1752
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-23 16:17:34 +0100

    removing instance methods as using class methods instead for fetching / saving data

--- models/seed.rb
+++ models/seed.rb
@@ -6,28 +6,28 @@ class Seed
   DB_NAME = 'dodas'
   TABLE_NAME = 'seeds'
 
-  attr_reader :id, :name, :db_name, :table_name, :settings
-
-  def initialize( data )
-
-    id = data['id'].to_i
-    @id = id if id != nil
-    @name = data['name']
-    @db_name = data['db_name']
-    @table_name = data['table_name']
-
-    settings_data = SqlInterface.all_where( DB_NAME, TABLE_NAME, { id: @id } )
-    @settings = ModelBuilder( SeedSetting, settings_data )
-  end
-
-  def save()
-    data = {
-      name: @name,
-      db_name: @db_name,
-      table_name: @table_name
-    }
-    id = SqlInterface.insert( DB_NAME, 'seeds', data )
-    @id = id
-  end
+  # attr_reader :id, :name, :db_name, :table_name, :settings
+  #
+  # def initialize( data )
+  #
+  #   id = data['id'].to_i
+  #   @id = id if id != nil
+  #   @name = data['name']
+  #   @db_name = data['db_name']
+  #   @table_name = data['table_name']
+  #
+  #   settings_data = SqlInterface.all_where( DB_NAME, TABLE_NAME, { id: @id } )
+  #   @settings = ModelBuilder( SeedSetting, settings_data )
+  # end
+  #
+  # def save()
+  #   data = {
+  #     name: @name,
+  #     db_name: @db_name,
+  #     table_name: @table_name
+  #   }
+  #   id = SqlInterface.insert( DB_NAME, 'seeds', data )
+  #   @id = id
+  # end
 
 end
--- models/seed_setting.rb
+++ models/seed_setting.rb
@@ -3,24 +3,26 @@ require_relative('../db/sql_interface')
 
 class SeedSetting
 
-  attr_reader :id, :seed_id, :column_name, :source_file
+  TABLE_NAME = 'seed_settings'
 
-  def initialize( data )
-    id = data['id'].to_i
-    @id = id if id != nil
-    @seed_id = data['seed_id'].to_i
-    @column_name = data['column_name']
-    @source_file = data['source_file']
-  end
-
-  def save()
-    data = {
-      seed_id: @seed_id,
-      column_name: @column_name,
-      source_file: @source_file
-    }
-    id = SqlInterface.insert( Seed::DB_NAME, 'seed_settings', data )
-    @id = id
-  end
+  # attr_reader :id, :seed_id, :column_name, :source_file
+  #
+  # def initialize( data )
+  #   id = data['id'].to_i
+  #   @id = id if id != nil
+  #   @seed_id = data['seed_id'].to_i
+  #   @column_name = data['column_name']
+  #   @source_file = data['source_file']
+  # end
+  #
+  # def save()
+  #   data = {
+  #     seed_id: @seed_id,
+  #     column_name: @column_name,
+  #     source_file: @source_file
+  #   }
+  #   id = SqlInterface.insert( Seed::DB_NAME, 'seed_settings', data )
+  #   @id = id
+  # end
 
 end
