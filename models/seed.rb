5a7ac7bd35ed6808aecd786353e550d3f6ad62fc
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-23 09:44:08 +0100

    changed initializer to use ModelBuilder class

--- models/seed.rb
+++ models/seed.rb
@@ -1,20 +1,23 @@
 require_relative('../db/sql_interface')
+require_relative('./model_builder')
 
 class Seed
 
   DB_NAME = 'dodas'
+  TABLE_NAME = 'seeds'
 
   attr_reader :id, :name, :db_name, :table_name, :settings
 
   def initialize( data )
+
     id = data['id'].to_i
     @id = id if id != nil
     @name = data['name']
     @db_name = data['db_name']
     @table_name = data['table_name']
 
-    seed_settings_sql = "SELECT * FROM seed_settings WHERE seed_id = #{@id}"
-    @settings = SqlRunner.run( 'dodas', seed_settings_sql, true )
+    settings_data = SqlInterface.all_where( DB_NAME, TABLE_NAME, { id: @id } )
+    @settings = ModelBuilder( SeedSetting, settings_data )
   end
 
   def save()
