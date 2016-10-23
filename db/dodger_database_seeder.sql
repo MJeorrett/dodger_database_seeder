25a6c8d472e52bf8baab588b867f52a54bdce21f
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-23 16:56:24 +0100

    renaming columns for clarity

--- db/dodger_database_seeder.sql
+++ db/dodger_database_seeder.sql
@@ -4,13 +4,13 @@ DROP TABLE IF EXISTS seeds;
 CREATE TABLE seeds (
   id SERIAL4 primary key,
   seed_name VARCHAR(100),
-  db_name VARCHAR(100),
-  table_name VARCHAR(100)
+  target_database VARCHAR(100),
+  target_table VARCHAR(100)
 );
 
 CREATE TABLE seed_settings (
   id SERIAL4 primary key,
   seed_id INT4 REFERENCES seeds(id),
-  column_name VARCHAR(100),
+  target_column VARCHAR(100),
   source_file VARCHAR(255)
 );
