af283ccbbabb753c3085f5a01f4c7b5ce8d823d3
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-23 16:58:27 +0100

    adding methods to return all seeds and all settings for a seed

--- models/seed.rb
+++ models/seed.rb
@@ -1,33 +1,18 @@
 require_relative('../db/sql_interface')
-require_relative('./model_builder')
 
 class Seed
 
   DB_NAME = 'dodas'
   TABLE_NAME = 'seeds'
 
-  # attr_reader :id, :name, :db_name, :table_name, :settings
-  #
-  # def initialize( data )
-  #
-  #   id = data['id'].to_i
-  #   @id = id if id != nil
-  #   @name = data['name']
-  #   @db_name = data['db_name']
-  #   @table_name = data['table_name']
-  #
-  #   settings_data = SqlInterface.all_where( DB_NAME, TABLE_NAME, { id: @id } )
-  #   @settings = ModelBuilder( SeedSetting, settings_data )
-  # end
-  #
-  # def save()
-  #   data = {
-  #     name: @name,
-  #     db_name: @db_name,
-  #     table_name: @table_name
-  #   }
-  #   id = SqlInterface.insert( DB_NAME, 'seeds', data )
-  #   @id = id
-  # end
+  def self.all_for_table_in_database( table_name, db_name )
+
+    conditions = {
+      target_table: table_name,
+      target_database: db_name
+    }
+    seeds_data = SqlInterface.all_where( DB_NAME, TABLE_NAME, conditions )
+    return seeds_data
+  end
 
 end
--- models/seed_setting.rb
+++ models/seed_setting.rb
@@ -1,28 +1,13 @@
-require_relative('./seed')
 require_relative('../db/sql_interface')
 
 class SeedSetting
 
+  DB_NAME = 'dodas'
   TABLE_NAME = 'seed_settings'
 
-  # attr_reader :id, :seed_id, :column_name, :source_file
-  #
-  # def initialize( data )
-  #   id = data['id'].to_i
-  #   @id = id if id != nil
-  #   @seed_id = data['seed_id'].to_i
-  #   @column_name = data['column_name']
-  #   @source_file = data['source_file']
-  # end
-  #
-  # def save()
-  #   data = {
-  #     seed_id: @seed_id,
-  #     column_name: @column_name,
-  #     source_file: @source_file
-  #   }
-  #   id = SqlInterface.insert( Seed::DB_NAME, 'seed_settings', data )
-  #   @id = id
-  # end
+  def self.all_settings_for_seed_id( seed_id )
+    settings_data = SqlInterface.all_where( DB_NAME, TABLE_NAME, { seed_id: seed_id } )
+    return settings_data
+  end
 
 end
