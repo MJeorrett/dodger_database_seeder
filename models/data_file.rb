c29c31785759774e0f53a3f9fa3951c8a73b04af
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-24 09:15:08 +0100

    return Pathname objects

--- models/data_file.rb
+++ models/data_file.rb
@@ -1,8 +1,12 @@
 class DataFile
 
   def self.all_names()
-    file_names = Dir[ENV['HOME'] + "/data/*"]
-    binding.pry
+    file_paths = Dir[ENV['HOME'] + "/data/*"]
+
+    file_names = file_paths.map do |file_path|
+      Pathname.new(file_path).basename()
+    end
+
     return file_names
   end
 
