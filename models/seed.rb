90e0e67e2dc6454e667d55cc8015f3722c474186
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-24 11:36:02 +0100

    adding save method

--- models/seed.rb
+++ models/seed.rb
@@ -1,6 +1,7 @@
 require_relative('../db/sql_interface')
 require_relative('./model_builder')
 require_relative('./html_element')
+require_relative('./seed_setting')
 
 class Seed
 
@@ -32,6 +33,25 @@ class Seed
     seeds_data = SqlInterface.all_where( DB_NAME, TABLE_NAME, conditions )
     seeds_models = ModelBuilder.models_from_data( Seed, seeds_data )
     return seeds_models
+  end
+
+  def self.save( data, target_database, target_table, target_columns )
+    values_hash = {
+      'seed_name' => data[:seed_name],
+      'target_database' => target_database,
+      'target_table' => target_table
+    }
+    seed_id = SqlInterface.insert( DB_NAME, TABLE_NAME, values_hash )
+
+    target_columns.each do |target_column|
+      setting_data = {
+        'seed_id' => seed_id,
+        'target_column' => target_column,
+        'source_file' => data[target_column]
+      }
+      seed_setting = Seed.new( setting_data )
+      seed_setting.save()
+    end
   end
 
 end
