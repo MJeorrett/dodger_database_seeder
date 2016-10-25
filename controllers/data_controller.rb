e85a48ffb877f4b67a14bce3eae7b9ab9f053ebb
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-25 21:44:19 +0100

    adding controller outline

---
+++ controllers/data_controller.rb
@@ -0,0 +1,37 @@
+require('sinatra')
+require('sinatra/contrib/all')
+
+# INDEX
+get '/databases/:db_name/tables/:table_name/data' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databases/:db_name/tables/:table_name/data' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+# NEW
+get '/databases/:db_name/tables/:table_name/data/new' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databases/:db_name/tables/:table_name/data/new' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+# CREATE
+post '/databases/:db_name/tables/:table_name/data' do
+  "Error 600: Matthew laziness error<br /><br />POST '/databases/:db_name/tables/:table_name/data' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+# SHOW
+get '/databases/:db_name/tables/:table_name/data/:row_id' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databases/:db_name/tables/:table_name/data/:row_id' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+# EDIT
+get '/databases/:db_name/tables/:table_name/data/:row_id/edit' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databases/:db_name/tables/:table_name/data/:row_id/edit' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+# UPDATE
+put '/databases/:db_name/tables/:table_name/data/:row_id' do
+  "Error 600: Matthew laziness error<br /><br />PUT '/databases/:db_name/tables/:table_name/data/:row_id' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+# DESTROY
+delete '/databases/:db_name/tables/:table_name/data/:row_id' do
+  "Error 600: Matthew laziness error<br /><br />DELETE '/databases/:db_name/tables/:table_name/data/:row_id' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
