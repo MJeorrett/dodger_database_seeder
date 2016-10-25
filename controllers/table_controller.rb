254e09e8cec7b194d9326610ad89f58c9ae76f59
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-25 15:33:38 +0100

    adding tables controller

---
+++ controllers/table_controller.rb
@@ -0,0 +1,37 @@
+require('sinatra')
+require('sinatra/contrib/all')
+
+# INDEX
+get '/databases/:dbname/tables' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databases/:dbname/tables' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+# NEW
+get '/databases/:dbname/tables/new' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databases/:dbname/tables/new' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+# CREATE
+post '/databases/:dbname/tables' do
+  "Error 600: Matthew laziness error<br /><br />POST '/databases/:dbname/tables' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+# SHOW
+get '/databases/:dbname/tables/:table_name' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databases/:dbname/tables/:table_name' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+# EDIT
+get '/databases/:dbname/tables/:table_name/edit' do
+  "Error 600: Matthew laziness error<br /><br />GET '/databases/:dbname/tables/:table_name/edit' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+# UPDATE
+put '/databases/:dbname/tables/:table_name' do
+  "Error 600: Matthew laziness error<br /><br />PUT '/databases/:dbname/tables/:table_name' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
+
+# DESTROY
+delete '/databases/:dbname/tables/:table_name' do
+  "Error 600: Matthew laziness error<br /><br />DELETE '/databases/:dbname/tables/:table_name' not implemented yet ... :-(<br /><br /><hr><br />params passed: #{params}"
+end
