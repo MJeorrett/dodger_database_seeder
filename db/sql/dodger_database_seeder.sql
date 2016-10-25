56e26414388fe0745e74bb218d3f017c8449b45f
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-25 11:56:49 +0100

    adding not null constraints

--- db/sql/dodger_database_seeder.sql
+++ db/sql/dodger_database_seeder.sql
@@ -5,15 +5,15 @@ DROP TABLE IF EXISTS seeds;
 
 CREATE TABLE seeds (
   id SERIAL4 primary key,
-  seed_name VARCHAR(100),
-  target_database VARCHAR(100),
-  target_table VARCHAR(100)
+  seed_name VARCHAR(100) not null,
+  target_database VARCHAR(100) not null,
+  target_table VARCHAR(100) not null
 );
 
 CREATE TABLE seed_settings (
   id SERIAL4 primary key,
-  seed_id INT4 REFERENCES seeds(id),
-  target_column VARCHAR(100),
+  seed_id INT4 REFERENCES seeds(id) not null,
+  target_column VARCHAR(100) not null,
   source_file VARCHAR(255),
   min DECIMAL(25, 10),
   max DECIMAL(25, 10),
